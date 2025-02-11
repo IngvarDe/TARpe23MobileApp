eec16e603b513c5d7fac758e0bcf8fcbdfb68a81
Ingvar Derešivski <inde999@gmail.com>
2025-02-11 13:51:26 +0200

    PlanetsPageSelection

--- MauiPlanets/MauiPlanets/Views/PlanetsPage.xaml.cs
+++ MauiPlanets/MauiPlanets/Views/PlanetsPage.xaml.cs
@@ -1,9 +1,21 @@
+using Services;
+
 namespace Views;
 
 public partial class PlanetsPage : ContentPage
 {
+	private const uint AnimationDuration = 800u;
+
 	public PlanetsPage()
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+		lstPopularPlanets.ItemsSource = PlanetsService.GetFeaturedPlanets();
+		lstAllPlanets.ItemsSource = PlanetsService.GetAllPlanets();
+    }
 }
